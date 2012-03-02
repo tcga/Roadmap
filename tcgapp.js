f58c6cf832e35aa6c5342ee3954aad48aadf1a8e
David Robbins <robbins.david@gmail.com>
2012-03-02 12:33:19 -0600

    Initial scraper draft

--- tcgapp.js
+++ tcgapp.js
@@ -11,7 +11,7 @@
 
     init : function init(){
       console.log("Scrape Started");
-      this.store = new rdfstore.Store({ persistent:true, name:"tgca-rdf", overwrite:"false"}, function(store){
+      this.store = new rdfstore.Store(function(store){
         store.registerDefaultProfileNamespaces();
         store.registerDefaultNamespace("tcga", "http://tcga/#");
       });
@@ -19,35 +19,50 @@
 
     store : null,
 
-    levels : {
+    types : {
       9 : "tcga:disease-study",
       10 : "tcga:center-type",
       11 : "tcga:center-domain",
       12 : "tcga:platform",
-      13 : "tcga:data-type"
+      13 : "tcga:data-type",
+      14 : "tcga:archive",
+      file : "tcga:file"
     },
 
-    scrape : function(url){
-      var target = url || "https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/",
+    scrape : function(url, parentId){
+      var TCGARoot = "https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/",
+          target = url || TCGARoot,
           store = this.store,
-          type = this.levels[target.split("/").length];
+          types = this.types;
       TCGA.get(target, function(error, response){
         if (!error){
-          var $links = $("a", $(response)),
-              querystring = "";
+          var $links = $("a", $(response));
           $links.each(function(i, link){
             var name = $(link).attr("href"),
-                id = uuid();
+                id = uuid(),
+                url = target + name,
+                querystring = "",
+                type = types[target.split("/").length];
             if(name.match(/^[\?\/]/)) {
               return;
             }
+            if(name.match(/^.*\.[^\/]+$/)){
+              type = types.file;
+            }
             name = name.split("/")[0];
+            querystring += "tcga:"+id+" tcga:url '"+url+"' .\n";
             querystring += "tcga:"+id+" tcga:type "+type+" .\n";
-            querystring += "tcga:"+id+" tcga:study-abbreviation tcga:"+name.toUpperCase()+" .\n";
-          });
-          store.execute("INSERT DATA {"+querystring+"}", function(success, results){
-            if(!success){
-              console.error(results);
+            querystring += "tcga:"+id+" tcga:ftp-name '"+name+"' .\n";
+            if(parentId){
+              querystring += "tcga:"+id+" tcga:parent tcga:"+parentId+" .\n";
+            }
+            store.execute("INSERT DATA {"+querystring+"}", function(success, results){
+              if(!success){
+                console.error(results);
+              }
+            });
+            if (type !== types.file){
+              TCGA.Scraper.scrape(url, id);
             }
           });
         }
@@ -58,7 +73,7 @@
   //Load a few modules and bootstrap the application
   TCGA.loadScript("https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js", function(error){
     if (!error){
-      TCGA.loadScript("https://github.com/antoniogarrote/rdfstore-js/raw/master/dist/browser_persistent/rdf_store_min.js", function(error){
+      TCGA.loadScript("https://github.com/antoniogarrote/rdfstore-js/raw/master/dist/browser/rdf_store_min.js", function(error){
         if(!error){
           TCGAScraper.init();
           TCGA.Scraper = TCGA.Scraper || TCGAScraper;
