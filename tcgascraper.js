b63cd84a26a4da58bea5e039d877684661cb79aa
David Robbins <robbins.david@gmail.com>
2012-04-25 16:41:03 +0000

    Sample from Cloud9

--- tcgascraper.js
+++ tcgascraper.js
@@ -124,7 +124,7 @@
 
     scrapeList : null,
 
-    gui : '<div class="span12"><h1>TCGA SPARQL Interface</h1></div><form class="span6" id="query"><div class="control-group"><div class="controls"><textarea class="span6" id="sparql" rows="10">SELECT * WHERE { ?s ?p ?o . } LIMIT 25</textarea><span class="help-inline"><p>Enter our SPARQL Query and click submit.</p></span></div></div><div class="form-actions"><button type="submit" class="btn btn-primary">Submit Query</button> <button class="btn">Cancel</button></div></form><div id="controls" class="span6"><div id="message"></div><div id="scrapelist"></div></div><div class="span12" id="results"></div>',
+    gui : '<div class="span12"><h1>TCGA SPARQL Interfacey</h1></div><form class="span6" id="query"><div class="control-group"><div class="controls"><textarea class="span6" id="sparql" rows="10">SELECT * WHERE { ?s ?p ?o . } LIMIT 25</textarea><span class="help-inline"><p>Enter our SPARQL Query and click submit.</p></span></div></div><div class="form-actions"><button type="submit" class="btn btn-primary">Submit Query</button> <button class="btn">Cancel</button></div></form><div id="controls" class="span6"><div id="message"></div><div id="scrapelist"></div></div><div class="span12" id="results"></div>',
 
     nav : 'SPARQL',
 
