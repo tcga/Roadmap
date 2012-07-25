7a53c2fb932a513cafc7a5b157988bfbd671cbac
David Robbins <robbins.david@gmail.com>
2012-07-25 07:30:01 -0500

    Fixed a whitespace issue.

--- scraper.js
+++ scraper.js
@@ -19,24 +19,24 @@
 
   // Semaphore from https://gist.github.com/1296828
   Sync = function Sync(syncCount, callback, preventInstantCallbackExecution) {
-      this.syncCount = syncCount;
-      this.callback = callback;
-      if(preventInstantCallbackExecution === false && this.syncCount === 0) {
-        this.executeCallback();
-      }
-    };
-    Sync.prototype.decrement = function() {
-      --this.syncCount;
-      if(this.syncCount === 0) {
-        this.executeCallback();
-      }
-    };
-    Sync.prototype.increment = function() {
-      ++this.syncCount;
-    };
-    Sync.prototype.executeCallback = function() {
-      if (typeof this.callback === "function") this.callback();
-    };
+    this.syncCount = syncCount;
+    this.callback = callback;
+    if(preventInstantCallbackExecution === false && this.syncCount === 0) {
+      this.executeCallback();
+    }
+  };
+  Sync.prototype.decrement = function() {
+    --this.syncCount;
+    if(this.syncCount === 0) {
+      this.executeCallback();
+    }
+  };
+  Sync.prototype.increment = function() {
+    ++this.syncCount;
+  };
+  Sync.prototype.executeCallback = function() {
+    if (typeof this.callback === "function") this.callback();
+  };
 
   // Writer Singleton
   Writer = (function (){
